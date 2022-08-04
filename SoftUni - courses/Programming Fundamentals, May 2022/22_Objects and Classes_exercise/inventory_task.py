2aa55394633f2898b1b938332a2541fa576447f9
habitant <ralitsa.ivanova17@gmail.com>
2022-08-05 00:38:07 +0300

    Materials and homework from the "Object and class - exercise" lecture.

---
+++ SoftUni - courses/Programming Fundamentals, May 2022/22_Objects and Classes_exercise/inventory_task.py
@@ -0,0 +1,31 @@
+class Inventory:
+
+    def __init__(self, __capacity: int):
+        self.capacity = __capacity
+        self.items = []
+        self.left_capacity = self.capacity
+
+    def add_item(self, item: str):
+        if self.left_capacity > 0:
+            self.items.append(item)
+            self.left_capacity -= 1
+        return "not enough room in the inventory"
+
+    def get_capacity(self):
+        return self.capacity
+
+    def __repr__(self):
+        final_items = ", ".join(self.items)
+        return f"Items: {final_items}.\nCapacity left: {self.left_capacity}"
+
+inventory = Inventory(2)
+
+inventory.add_item("potion")
+
+inventory.add_item("sword")
+
+print(inventory.add_item("bottle"))
+
+print(inventory.get_capacity())
+
+print(inventory)
